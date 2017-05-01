11f0eb21ef660978e10b9dc5ca4ac16c0a7f5f58
Azure616 <bk7bb@virginia.edu>
2017-04-30 21:40:28 -0400

    Correct image loading

--- train.py
+++ train.py
@@ -12,33 +12,32 @@ from skimage import io
 # (342, 2270, 1)
 
 
-def reduced_model(num_of_class=-1, input_shape=(342, 2270, 1)):
+def basic_model(num_of_class=-1, input_shape=(342, 2270, 1)):
     model = Sequential()
-    print("Input_shape: {}".format(input_shape))
+    #print("Input_shape: {}".format(input_shape))
     model.add(Conv2D(64,
                      input_shape=input_shape,
                      data_format='channels_last',
                      kernel_size=(3,3),
                      activation='relu',
                      ))
-    print("From Conv2D-1: {}".format(model.output_shape))
+    #print("From Conv2D-1: {}".format(model.output_shape))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2,2)))
-    print("From MaxP-1: {}".format(model.output_shape))
+    #print("From MaxP-1: {}".format(model.output_shape))
     model.add(Conv2D(64,
                      kernel_size=(3, 3),
                      activation='relu',
                      ))
-    print("From Conv2D-2: {}".format(model.output_shape))
+    #print("From Conv2D-2: {}".format(model.output_shape))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    print("From MaxP-2: {}".format(model.output_shape))
-    model.add(Dense(1))
-    model.add(Activation('relu'))
-    print("Activation: {}".format(model.output_shape))
+    #print("From MaxP-2: {}".format(model.output_shape))
+    model.add(Dense(1, activation='relu'))
+    #print("Activation: {}".format(model.output_shape))
     model.add(Flatten())
     model.add(Reshape((21, 2264,)))
-    print("Reshape: {}".format(model.output_shape))
+    #print("Reshape: {}".format(model.output_shape))
     model.add(Bidirectional(LSTM(64,
                                  activation='relu'),
                             merge_mode='concat'))
@@ -47,12 +46,12 @@ def reduced_model(num_of_class=-1, input_shape=(342, 2270, 1)):
     print("RepeatVector: {}".format(model.output_shape))
     model.add(LSTM(64,
                    activation='relu'))
-    model.add(Dense(num_of_class))
-    model.add(Activation("softmax"))
+    model.add(Dense(num_of_class, activation='softmax'))
     return model
 
+
 # Split data into training, testing and validation sets and load them
-def split_dataset_rand(paths, labels, train_prop=0.2, test_prop=0.1, val = False):
+def split_dataset_rand(paths, labels, train_prop=0.85, test_prop=0.15, val = False):
     print("Preparing training, testing and validation datasets...")
     if len(paths) != len(labels):
         print("Fatal: data missing and not aligned")
@@ -66,15 +65,15 @@ def split_dataset_rand(paths, labels, train_prop=0.2, test_prop=0.1, val = False
     validation_indices = indices[train_size+test_size:]
     X = preprocess_images(paths)
     Y = labels
-    Xtrain = [X[i] for i in training_indices]
-    Ytrain = [Y[i] for i in training_indices]
-    Xtest = [X[i] for i in testing_indices]
-    Ytest = [Y[i] for i in testing_indices]
+    Xtrain = np.asarray([X[i] for i in training_indices])
+    Ytrain = np.asarray([Y[i] for i in training_indices])
+    Xtest = np.asarray([X[i] for i in testing_indices])
+    Ytest = np.asarray([Y[i] for i in testing_indices])
     Xval = None
     Yval = None
     if leng > train_size+test_size and val:
-        Xval = preprocess_images([paths[i] for i in validation_indices])
-        Yval = [labels[i] for i in validation_indices]
+        Xval = np.asarray([X[i] for i in validation_indices])
+        Yval = np.asarray([labels[i] for i in validation_indices])
         print("Training Set: {} | Testing Set: {} | Validation Set: {}".format(train_size, test_size, leng-(train_size+test_size)))
         return Xtrain, Ytrain, Xtest, Ytest, Xval, Yval
     else:
@@ -155,42 +154,28 @@ def preprocess_images(paths):
 
 # Invoke this to build and train the model
 def train():
-    #paths, labels, dic = preprocess_labels()
-    #Xtrain, Ytrain, Xtest, Ytest = split_dataset_rand(paths, labels)
-    batch_size = 16
+    paths, labels, dic = preprocess_labels()
+    Xtrain, Ytrain, Xtest, Ytest = split_dataset_rand(paths, labels)
+    batch_size = 1
     epoch = 2
 
-    #input_shape = Xtrain[0].shape
-    #print(input_shape)
-
-    model = reduced_model(num_of_class=81)
+    model = basic_model(num_of_class=81)
     print("Compiling...")
     model.compile(optimizer=Adadelta(),
                   loss=categorical_crossentropy,
                   metrics=['accuracy'])
-    '''
+
     print("Training...")
-    train_size = len(Xtrain)
-    bundle_size = 64
-    start, end = 0, 1
-    iteration = int(train_size / bundle_size)
-    for i in range(iteration):
-        end += bundle_size
-        model.fit(x=Xtrain[start:end],
-                  y=Ytrain[start:end],
-                  verbose=1,
-                  batch_size=batch_size,
-                  epochs=epoch)
-    if end < train_size:
-        model.fit(x=Xtrain[end:],
-                  y=Ytrain[end:],
-                  verbose=1,
-                  batch_size=batch_size,
-                  epochs=epoch)
+    model.fit(x=Xtrain,
+              y=Ytrain,
+              verbose=1,
+              batch_size=batch_size,
+              epochs=epoch)
     model.evaluate(x=Xtest,
                    y=Ytest)
+
     model.save(filepath='./model.h5')
-    '''
+
 
 if __name__ == '__main__':
     #test = ['a01-000u-00', 'a01-000u-01', 'a01-000u-02', 'a01-000u-03', 'a01-000u-04']
